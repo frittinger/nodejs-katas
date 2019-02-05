024dd691607c6c932d41b48057e6f4ae3b3eee57
Frank Rittinger <frank.rittinger@virtual-identity.com>
2019-02-05 16:44:12 +0100

    added first TDD code kata for string calculator

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,4 +1,23 @@
 function addTwoNumbers(x, y) {
     return x + y;
 }
-module.exports = addTwoNumbers;
+
+function addNumberAndString(n, s) {
+    return n + parseInt(s);
+}
+
+/*
+takes 0 to 2 numbers and return their sum with 0 for the empty string
+ */
+function add(s) {
+    if (s !== null && s !== '') {
+        splitValues = s.split(/,|\n/);
+        return splitValues.reduce((a, b) => a + Number(b), 0);
+    } else {
+        return 0;
+    }
+}
+
+module.exports.addTwoNumbers = addTwoNumbers;
+module.exports.addNumberAndString = addNumberAndString;
+module.exports.add = add;
