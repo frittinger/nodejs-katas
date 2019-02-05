54a1a7c43488843bf183f804ef3c4ec95add8efc
Frank Rittinger <frank.rittinger@virtual-identity.com>
2019-02-05 16:44:31 +0100

    added first TDD code kata for string calculator

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var addTwoNumbers = require('./addTwoNumbers');
 
-console.log(addTwoNumbers(1,2));
+console.log(addTwoNumbers.addTwoNumbers(1, 2));
+
+console.log(17 + parseInt("32"));
+console.log(parseInt("cat"));
+
+var str = "123, 124, 234,252";
+var arr = str.split(",").map(val => Number(val) + 1);
+console.log(arr);
+
+console.log(addTwoNumbers.addNumberAndString(2, "2"));
--- test/addTwoNumbers.test.js
+++ test/addTwoNumbers.test.js
@@ -1,6 +1,29 @@
 const addTwoNumbers = require('../addTwoNumbers');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(addTwoNumbers(1, 2)).toBe(3);
+    expect(addTwoNumbers.addTwoNumbers(1, 2)).toBe(3);
 });
 
+test('adds 1 + "1" to equal 2', () => {
+    expect(addTwoNumbers.addNumberAndString(1, "1")).toBe(2);
+});
+
+test('add nothing', () => {
+    expect(addTwoNumbers.add("")).toBe(0);
+});
+
+test('one number returns itself', () => {
+    expect(addTwoNumbers.add("7")).toBe(7);
+});
+
+test('add two numbers', () => {
+    expect(addTwoNumbers.add("7,12")).toBe(19);
+});
+
+test('add 5 numbers', () => {
+    expect(addTwoNumbers.add("1,2,3,4,5")).toBe(15);
+});
+
+test('add 5 numbers with , or \\n as seperators', () => {
+    expect(addTwoNumbers.add("1,2\n3,4\n5")).toBe(15);
+});
